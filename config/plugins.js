57d03520d549e44497b02a1a7ead8b7339b53f4f
Randyhall91 <randyrocks11@gmail.com>
2022-12-05 21:22:33 -0700

    take aws_ off REGION and ACCESS_KEY_ID in env

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        accessKeyId: env('ACCESS_KEY_ID'),
         secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
+        region: env('REGION'),
         params: {
           Bucket: env('AWS_BUCKET'),
         },
