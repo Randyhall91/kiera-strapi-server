7dba67ee1d48475a03b32d67583d256b73763def
Randyhall91 <randyrocks11@gmail.com>
2022-12-05 20:53:14 -0700

    AWS connection successful

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,21 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  /* Replace 'strapi::security', with this snippet */
+  /* Beginning of snippet */
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', `https://${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`],
+          'media-src': ["'self'", 'data:', 'blob:', `https://${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
